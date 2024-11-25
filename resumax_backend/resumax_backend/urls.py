ed62b175fdc92c3e10682d846a258b1824abdc3c
liamjdavis <davisliam123@gmail.com>
2024-11-25 14:49:11 -0800

    changed files to reference resumax_backend instead of matching_backend and added requirements.txt

--- resumax_backend/resumax_backend/urls.py
+++ resumax_backend/resumax_backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for matching_backend project.
+URL configuration for resumax_backend project.
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
--- resumax_backend/resumax_backend/wsgi.py
+++ resumax_backend/resumax_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for matching_backend project.
+WSGI config for resumax_backend project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
