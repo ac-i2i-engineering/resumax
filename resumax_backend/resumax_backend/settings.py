11460deab9e2d24955c2badfe40e64152332a731
liamjdavis <davisliam123@gmail.com>
2024-12-15 13:41:39 -0500

    Initialized Document model connected to the vectordb package

--- resumax_backend/resumax_backend/settings.py
+++ resumax_backend/resumax_backend/settings.py
@@ -24,6 +24,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resumax_algo',
+    'vectordb',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
