ae340833fca35111288654b1dbe9a526787f02f9
Yichen L <yliu2k@gmail.com>
2025-02-04 22:34:57 -0500

    added html webpage interface with js functionality, added framework for processing files uploaded to the page

--- resumax_backend/resumax_algo/views.py
+++ resumax_backend/resumax_algo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
+
+import os
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST' and request.FILES['file']:
+        print('FILE UPLOADED')
+        file = request.FILES['file']
+        fs = FileSystemStorage()
+        file_path = fs.save(file.name, file)
+        file_url = fs.url(file_path)
+
+        # Clean up the uploaded file
+        os.remove(file_path)
+        
+        return render(request, 'resumax_algo/index.html')
+
+    return render(request, 'resumax_algo/index.html')
