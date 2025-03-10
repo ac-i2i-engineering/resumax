b42f0e18be427d5725b08ebe2678f956778ca4e5
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-03-10 08:53:43 -0400

    resolve cicd error 4

--- resumax_backend/resumax_backend/settings.py
+++ resumax_backend/resumax_backend/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap_icons',
-    'rest_framework',
     'resumax_algo',
     'resumax_auth',
     'resumax_api',
