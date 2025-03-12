200f2f6d15592dffc1502b081d84b1c65e23759e
ndayiemile <ndayishimiyeemile96@gmail.com>
2025-03-12 00:28:02 -0400

    refactor: removed redudant requirements files. configured secret key storage with env vars

--- resumax_backend/resumax_backend/settings.py
+++ resumax_backend/resumax_backend/settings.py
@@ -7,6 +7,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
